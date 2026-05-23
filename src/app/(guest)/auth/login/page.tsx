b54a6e18425485687e2525cf83f8059bb757dee4
zana0422-creator <zana0422@g.eulji.ac.kr>
2026-05-23 20:36:52 +0900

    로그인 페이지 디자인 수정

--- src/app/(guest)/auth/login/page.tsx
+++ src/app/(guest)/auth/login/page.tsx
@@ -1,5 +1,8 @@
 import { Button } from "@/components/ui/button";
+import Image from "next/image";
 import Link from "next/link";
+import kakao from '@/app/assets/img/kakao.svg'
+import google from '@/app/assets/img/google.svg'
 
 export default function Login() {
 
@@ -14,20 +17,22 @@ export default function Login() {
                     <p className="text-white font-bold">수강생</p>
                 </div>
                 <div className="bg-mauve-200 py-2 text-center">
-                    <p className="text-black font-bold">강사</p>
+                    <p className="text-slate-900 font-bold">강사</p>
                 </div>
             </div>
 
             <form action="" className="flex flex-col gap-4">
                 <input
                     type="email"
-                    className="border border-black py-2 px-2"
+                    name="email"
+                    className="border border-slate-300 py-2 px-2"
                     placeholder="이메일 입력"
                 />
                 <div className="max-w-md">
                     <input
                         type="password"
-                        className="border border-black py-2 px-2 w-full"
+                        name="password"
+                        className="border border-slate-300 py-2 px-2 w-full"
                         placeholder="비밀번호 입력"
                     />
                     {nothingInFieldError ? (<p className="text-right mr-2 text-red-600 font-medium">{nothingInFieldError}</p>) : ("")}
@@ -35,17 +40,31 @@ export default function Login() {
                 <Button variant="default" className="w-auto py-6 cursor-pointer">로그인</Button>
             </form>
 
-            <hr className="border-0.5 border-black" />
-            <div className="bg-yellow-300 font-bold text-center py-2 cursor-pointer">
+            <hr className="border-0.5 border-slate-300" />
+            <div className="bg-yellow-300 text-slate-900 font-bold text-center py-2 cursor-pointer flex items-center justify-center gap-1.5">
+                <Image
+                    src={kakao}
+                    width={20}
+                    height={20}
+                    alt="카카오 아이콘"
+                    priority
+                />
                 <p>카카오톡으로 계속하기</p>
             </div>
-            <div className="border border-black font-bold text-center py-2 cursor-pointer">
+            <div className="border border-slate-700 text-slate-700 font-bold text-center py-2 cursor-pointer flex items-center justify-center gap-1.5">
+                <Image
+                    src={google}
+                    width={20}
+                    height={20}
+                    alt="구글 아이콘"
+                    priority
+                />
                 <p>구글로 계속하기</p>
             </div>
-            <hr className="border-0.5 border-black" />
+            <hr className="border-0.5 border-slate-300" />
             <div className="flex flex-row justify-center gap-7">
-                <p className="text-right">비밀번호 찾기</p>
-                <Link href='/auth/signup'><p className="text-left">회원가입</p></Link>
+                <p className="text-right text-slate-400">비밀번호 찾기</p>
+                <Link href='/auth/signup'><p className="text-left text-slate-400">회원가입</p></Link>
             </div>
         </div>
     );
