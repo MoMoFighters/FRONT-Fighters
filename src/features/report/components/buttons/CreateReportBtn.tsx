a67755eefff4aac9e1e87cb26c9321499f4a4517
zana0422-creator <zana0422@g.eulji.ac.kr>
2026-05-28 17:27:29 +0900

    [FEAT] 학생 메인페이지 #19

--- src/features/report/components/buttons/CreateReportBtn.tsx
+++ src/features/report/components/buttons/CreateReportBtn.tsx
@@ -1,69 +1,254 @@
 'use client'
 
+import { useState } from "react";
 import { Button } from "@/components/ui/button";
-import { Dialog, DialogClose, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
-import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
-import { Field, FieldGroup } from "@/components/ui/field";
+import {
+    Dialog,
+    DialogClose,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog";
+import {
+    Field,
+    FieldGroup,
+} from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { ChevronDown } from "lucide-react";
+import {
+    Select,
+    SelectContent,
+    SelectGroup,
+    SelectItem,
+    SelectLabel,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select";
+import TwoButtonModal from "@/features/modal/TwoButtonModal";
+import { toast } from "sonner";
+
+interface ReportFormData {
+    reason: string;
+    reasonDetail: string;
+}
 
 export default function CreateReportBtn() {
+
+    // 신고 작성 모달
+    const [isModal, setIsModal] = useState(false);
+
+    // 최종 확인 모달 상태 관리
+    const [isConfirmModal, setIsConfirmModal] = useState(false);
+
+    const [formData, setFormData] = useState<ReportFormData>({
+        reason: '',
+        reasonDetail: '',
+    });
+
+    // 신고하기 버튼 클릭
+    const handleOpenConfirmModal = (
+        e: React.FormEvent<HTMLFormElement>
+    ) => {
+
+        e.preventDefault();
+
+        // 간단 검증
+        if (!formData.reason) {
+            toast.error('신고 사유를 선택해주세요.', {
+                duration: 1000
+            });
+            return;
+        }
+
+        if (!formData.reasonDetail) {
+            toast.error('상세 내용을 작성해주세요.', {
+                duration: 1000
+            });
+            return;
+        }
+
+        setIsConfirmModal(true);
+    };
+
+    // 실제 신고 처리
+    const handleCreateReport = async () => {
+
+        try {
+
+            console.log(formData);
+
+            // 실제 API 호출
+            // await createReport(payload);
+
+            toast.success('신고 접수 성공', {
+                duration: 1000
+            });
+
+            // 상태 초기화
+            setFormData({
+                reason: '',
+                reasonDetail: '',
+            });
+
+            // 모달 닫기
+            setIsConfirmModal(false);
+            setIsModal(false);
+
+        } catch (error) {
+
+            toast.error('신고 접수 실패', {
+                duration: 1000
+            });
+        }
+    };
+
     return (
         <>
-            <Dialog>
-                <form>
-                    <DialogTrigger asChild>
-                        <Button variant="destructive" className='absolute w-15 bottom-5 right-5'>신고</Button>
-                    </DialogTrigger>
-                    <DialogContent className="sm:max-w-sm">
+            <Dialog
+                open={isModal}
+                onOpenChange={setIsModal}
+            >
+
+                <DialogTrigger asChild>
+                    <Button
+                        variant="destructive"
+                        className="absolute w-15 bottom-5 right-5"
+                    >
+                        신고
+                    </Button>
+                </DialogTrigger>
+
+                <DialogContent className="sm:max-w-sm">
+
+                    <form onSubmit={handleOpenConfirmModal}>
+
                         <DialogHeader>
-                            <DialogTitle className="text-slate-900 text-lg font-semibold">신고</DialogTitle>
-                            <DialogDescription className="text-xs whitespace-pre-line text-slate-400 leading-3">
-                                {`1. 해당 페이지에서 접수할 신고 사유를 작성해주세요.\n
-                                2. 악성 유저가 있다면 해당 유저의 닉네임도 적어주세요.\n
-                                3. 악성 게시글이 있다면 해당 게시글의 작성자 닉네임도 적어주세요.`}
+
+                            <DialogTitle
+                                className="text-red-400 text-lg font-bold"
+                            >
+                                신고
+                            </DialogTitle>
+
+                            <DialogDescription
+                                className="text-xs whitespace-pre-line text-slate-400 leading-5"
+                            >
+                                {`1. 해당 페이지에서 접수할 신고 사유를 작성해주세요.
+2. 악성 유저에 대한 신고라면 해당 유저의 닉네임도 적어주세요.
+3. 악성 게시글에 대한 신고라면 해당 게시글의 제목도 적어주세요.`}
                             </DialogDescription>
+
                         </DialogHeader>
-                        <FieldGroup>
+
+                        <FieldGroup className="mt-4">
+
                             <Field>
-                                <DropdownMenu>
-                                    <DropdownMenuTrigger asChild>
-                                        <Button variant="outline" className="relative text-slate-400 hover:text-slate-500 cursor-pointer">
-                                            신고 사유를 선택해주세요.
-                                            <ChevronDown className="absolute right-2 top-2" />
-                                        </Button>
-                                    </DropdownMenuTrigger>
-                                    <DropdownMenuContent>
-                                        <DropdownMenuGroup>
-                                            <DropdownMenuLabel>신고 사유</DropdownMenuLabel>
-                                            <DropdownMenuItem>홍보성 컨텐츠</DropdownMenuItem>
-                                            <DropdownMenuItem>도배성 컨텐츠</DropdownMenuItem>
-                                            <DropdownMenuItem>부적절한 언어</DropdownMenuItem>
-                                            <DropdownMenuItem>기타</DropdownMenuItem>
-                                        </DropdownMenuGroup>
-                                    </DropdownMenuContent>
-                                </DropdownMenu>
+
+                                <Select
+                                    value={formData.reason}
+                                    onValueChange={(value) =>
+                                        setFormData((prev) => ({
+                                            ...prev,
+                                            reason: value,
+                                        }))}
+                                >
+
+                                    <SelectTrigger className="w-full max-w-48 cursor-pointer">
+                                        <SelectValue placeholder="신고 사유를 선택해주세요." className="text-sm!" />
+                                    </SelectTrigger>
+
+                                    <SelectContent>
+
+                                        <SelectGroup>
+
+                                            <SelectLabel>
+                                                신고 사유
+                                            </SelectLabel>
+
+                                            <SelectItem value="SPAM">
+                                                홍보성 컨텐츠
+                                            </SelectItem>
+
+                                            <SelectItem value="ABUSE">
+                                                도배성 컨텐츠
+                                            </SelectItem>
+
+                                            <SelectItem value="INAPPROPRIATE">
+                                                부적절한 컨텐츠
+                                            </SelectItem>
+
+                                            <SelectItem value="ETC">
+                                                기타
+                                            </SelectItem>
+
+                                        </SelectGroup>
+
+                                    </SelectContent>
+
+                                </Select>
+
                             </Field>
+
                             <Field>
-                                <Label htmlFor="reason-detail">신고 사유</Label>
+
+                                <Label htmlFor="reasonDetail">
+                                    상세 내용
+                                </Label>
+
                                 <Input
-                                    id="reason-detail"
-                                    name="reason-detail"
-                                    placeholder="기타 사유 또는 닉네임을 작성해주세요."
+                                    id="reasonDetail"
+                                    placeholder="기타 사유 또는 닉네임 및 제목을 작성해주세요."
                                     className="text-xs!"
+                                    value={formData.reasonDetail}
+                                    onChange={(e) =>
+                                        setFormData((prev) => ({
+                                            ...prev,
+                                            reasonDetail: e.target.value,
+                                        }))}
                                 />
+
                             </Field>
+
                         </FieldGroup>
-                        <DialogFooter>
+
+                        <DialogFooter className="mt-6">
+
                             <DialogClose asChild>
-                                <Button variant="outline">Cancel</Button>
+                                <Button
+                                    variant="outline"
+                                    className="cursor-pointer"
+                                >
+                                    취소
+                                </Button>
                             </DialogClose>
-                            <Button type="submit">Save changes</Button>
+
+                            <Button
+                                type="submit"
+                                variant="destructive"
+                                className="cursor-pointer"
+                            >
+                                신고하기
+                            </Button>
+
                         </DialogFooter>
-                    </DialogContent>
-                </form>
+
+                    </form>
+
+                </DialogContent>
+
             </Dialog>
+
+            {/* 최종 확인 모달 */}
+            <TwoButtonModal
+                open={isConfirmModal}
+                onOpenChange={setIsConfirmModal}
+                title="신고를 접수하시겠습니까?"
+                description={`신고 후에는 취소할 수 없습니다.`}
+                onConfirm={handleCreateReport}
+            />
         </>
     );
 }
